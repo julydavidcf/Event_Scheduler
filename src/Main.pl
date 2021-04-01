9c08bc3ec9d1f79ec7da30153c0cee4de809a200
julydavidcf <50227264+julydavidcf@users.noreply.github.com>
2021-03-31 20:15:21 -0700

    1

--- src/Main.pl
+++ src/Main.pl
@@ -1,2 +1,5 @@
 event(start_time(H,M),Duration,Name,Year,Month,Date):-
-       integer(H),integer(M),integer(Duration),string(Name),integer(Year),integer(Month),integer(Date)
+       integer(H),integer(M),integer(Duration),string(Name),integer(Year),integer(Month),integer(Date).
+
+event(start_time(3,4),3,test,2021,3,31).
+
