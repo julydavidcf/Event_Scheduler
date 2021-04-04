074346676e7fbe0fda105c7e6c1829d93677dd58
luwenhua <luwenhua@students.cs.ubc.ca>
2021-04-03 17:33:51 -0700

    persistency library added

--- src/Main.pl
+++ src/Main.pl
@@ -1,46 +1,33 @@
-:- dynamic event/6.
+:- module(
+  event,
+  [
+    add_event/7, % +Hour:integer, +Minute:integer, +Duration:integer, +Name:atom, +Year:integer, +Month:integer, +Date:integer
+    delete_event/7, % +Hour:integer, +Minute:integer, +Duration:integer, +Name:atom, +Year:integer, +Month:integer, +Date:integer
+    delete_all_event/7, % +Hour:integer, +Minute:integer, +Duration:integer, +Name:atom, +Year:integer, +Month:integer, +Date:integer
+    current_event/7 % ?Hour:integer, ?Minute:integer, ?Duration:integer, ?Name:atom, ?Year:integer, ?Month:integer, ?Date:integer
+  ]
+).
 
-addValidEvent(event(start_time(H,M),Duration,Name,Year,Month,Date)):-
-    event(start_time(H,M),Duration,Name,Year,Month,Date),
-    write("Hey, this event already exists.").
+:- use_module(library(persistency)).
 
-addValidEvent(event(start_time(H,M),Duration,Name,Year,Month,Date)):-
-       integer(H),integer(M),integer(Duration),integer(Year),integer(Month),integer(Date),
-       H>=0,H=<24,M>=0,M=<59,Duration>0,Year>0,Month>0,Date>0,Month=<12,validDate(Year,Month,Date),
-       assert(event(start_time(H,M),Duration,Name,Year,Month,Date)).
+:- persistent(event(hour:integer,minute:integer,duration:integer,name:atom,year:integer,month:integer,date:integer)).
 
-addValidEvent(event(start_time(H,M),Duration,Name,Year,Month,Date)):-
-    write("Hey, don't put weird stuff :("),false.
-
-validDate(Y,2,D):- (X is mod(Y,4)),(D=<29),X==0.
-validDate(Y,2,D):- (X is mod(Y,4)),(D=<28).
-validDate(_,M,D):- (X is mod(M,2)),(D=<31),(M<8),X==1.
-validDate(_,M,D):- (X is mod(M,2)),(D=<31),(M>=8),X==0.
-validDate(_,M,D):- (X is mod(M,2)),(D=<30),(M>=8),X==1.
-validDate(_,M,D):- (X is mod(M,2)),(D=<30),(M<8),(M-2>0),X==0.
-
-removeEvent(H,M,Duration,Name,Year,Month,Date):-
-    retract(event(start_time(H,M),Duration,Name,Year,Month,Date)).
-
-listEvent:-
-    event(start_time(H,M),Duration,Name,Year,Month,Date),
-    write(Name),write(" starts at "),write(Year),write("-"),write(Month),write("-"),
-    write(Date),write(" at "),write(H),write(":"),write(M),write(" and lasts for "),write(Duration),
-    write(" hour(s)").
-
-todaysEvent:-
-    today(Year,Month,Date),
-    event(start_time(H,M),Duration,Name,Year,Month,Date),
-    write(Name),write(" starts today at "),write(H),write(":"),write(M),write(" and lasts for "),write(Duration),
-    write(" hour(s)").
+:- initialization(db_attach('scheduleKeeper.pl',[])).
 
 
-today(Year,Month,Day) :-
-    get_time(T),
-    date_time_value(year, DateTime, Year),
-    date_time_value(month, DateTime, Month),
-    date_time_value(day, DateTime, Day),
-    stamp_date_time(T, DateTime, local).
+add_event(Hour,Minute,Duration,Name,Year,Month,Date):-
+   with_mutex(event_db, assert_event(Hour,Minute,Duration,Name,Year,Month,Date)).
+
+current_event(Hour,Minute,Duration,Name,Year,Month,Date):-
+    with_mutex(event_db,event(Hour,Minute,Duration,Name,Year,Month,Date)).
+
+
+delete_event(Hour,Minute,Duration,Name,Year,Month,Date):-
+   with_mutex(event_db, retract_event(Hour,Minute,Duration,Name,Year,Month,Date)).
+
+delete_all_event(Hour,Minute,Duration,Name,Year,Month,Date):-
+   with_mutex(event_db, retractall_event(Hour,Minute,Duration,Name,Year,Month,Date)).
+
 
 createEvent(R):-
     write("What is this event?   "),
@@ -71,9 +58,51 @@ createEvent(R):-
     flush_output(current_output),
     readln([Ln6|X]),
 
-    %write(Ln),write(Ln1),write(Ln2),write(Ln3),write(Ln4),write(Ln5),write(Ln6),
-    addValidEvent(event(start_time(Ln1,Ln2),Ln3,Ln,Ln4,Ln5,Ln6)).
+    addValidEvent(event(Ln1,Ln2,Ln3,Ln,Ln4,Ln5,Ln6)),
+     add_event(Ln1,Ln2,Ln3,Ln,Ln4,Ln5,Ln6).
+
+addValidEvent(event(H,M,Duration,Name,Year,Month,Date)):-
+    event(H,M,Duration,Name,Year,Month,Date),
+    write("Hey, this event already exists.").
+
+addValidEvent(event(H,M,Duration,Name,Year,Month,Date)):-
+   integer(H),integer(M),integer(Duration),integer(Year),integer(Month),integer(Date), H>=0,H=<24,M>=0,M=<59,Duration>0,Year>0,Month>0,Date>0,Month=<12,validDate(Year,Month,Date),
+  assert(event(H,M,Duration,Name,Year,Month,Date)).
 
 
-%event(start_time(3,4),3,test,2021,3,31).
+addValidEvent(event(H,M,Duration,Name,Year,Month,Date)):-
+    write("Hey, don't put weird stuff :("),false.
+
+validDate(Y,2,D):- (X is mod(Y,4)),(D=<29),X==0.
+validDate(Y,2,D):- (X is mod(Y,4)),(D=<28).
+validDate(_,M,D):- (X is mod(M,2)),(D=<31),(M<8),X==1.
+validDate(_,M,D):- (X is mod(M,2)),(D=<31),(M>=8),X==0.
+validDate(_,M,D):- (X is mod(M,2)),(D=<30),(M>=8),X==1.
+validDate(_,M,D):- (X is mod(M,2)),(D=<30),(M<8),(M-2>0),X==0.
+
+removeEvent(H,M,Duration,Name,Year,Month,Date):-
+    retract(event(H,M,Duration,Name,Year,Month,Date)).
+    delete_event(H,M,Duration,Name,Year,Month,Date).
+
+listEvent:-
+    event(H,M,Duration,Name,Year,Month,Date),
+    write(Name),write(" starts at "),write(Year),write("-"),write(Month),write("-"),
+    write(Date),write(" at "),write(H),write(":"),write(M),write(" and lasts for "),write(Duration),
+    write(" hour(s)").
+
+todaysEvent:-
+    today(Year,Month,Date),
+    event(H,M,Duration,Name,Year,Month,Date),
+    write(Name),write(" starts today at "),write(H),write(":"),write(M),write(" and lasts for "),write(Duration),
+    write(" hour(s)").
+
+
+today(Year,Month,Day) :-
+    get_time(T),
+    date_time_value(year, DateTime, Year),
+    date_time_value(month, DateTime, Month),
+    date_time_value(day, DateTime, Day),
+    stamp_date_time(T, DateTime, local).
+
+
 
